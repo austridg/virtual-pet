8b4f0d859748cba32dab4ce4429a9cbea6ff73d4
Austin Ridge <amridge03@gmail.com>
2025-11-09 19:30:44 -0600

    added level stats

--- Pet.h
+++ Pet.h
@@ -1,11 +1,12 @@
 #include <SFML/Graphics.hpp>
+#include <SFML/System/Clock.hpp>
 
 enum class Mood { HAPPY, SAD, MAD, TIRED, ILL };
 enum class Animal { CAT, DOG, BIRD, HAMSTER, FOX, FISH, RABBIT};
 
 class Pet {
 private:
-    sf::CircleShape petGraphic;
+    sf::CircleShape petGraphic; // temporary
     Animal animal;
     Mood mood;
     int level;
@@ -24,11 +25,16 @@ public:
 
     Animal getAnimal() const;
     Mood getMood() const;
+    int getLevel() const;
+    int getExp() const;
+    int getNextLevelExp() const;
     int getAge() const;
+
     float getHunger() const;
     float getEnergy() const;
     float getCleanliness() const;
     float getAttention() const;
+
     float getMoodBar() const;
 };
 
